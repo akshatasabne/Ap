5299d2eaf588fb381447875ddef1bf911f4fa05d
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-07-04 11:50:46 +0530

    Add project files.

---
+++ Ap/Ap3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ap
+{
+    internal class Ap3
+    {
+        //Move zeros to beginning or end of array. 
+        //    e.g. if given array is { 2 , 10 , 0 ,5 ,3, 0, 4 ,1}
+        //then output array should be
+        //    a. { 0,0,2,10,5,3,4,1}
+        //b. {2,10,5,3,4,1,0,0}
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 10, 0, 5, 3, 0, 4, 1 };
+        }
+    }
+}
---
+++ Ap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. WAP to replace all negative value with its immediate left elements square. Means arr[] = [12, 3,-19, 29, 5, -61, 44, 7, -9]
+            //Output array will be [12, 3, 9, 29, 5, 25, 44, 7, 49].
+            int[] arr = new int[9] { 12, 3, -19, 29, 5, -61, 44, 7, -9 };
+            for(int i=0; i<arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    //  Console.WriteLine(arr[i]);
+                    int a = i - 1;
+                    arr[i] = arr[a] * arr[a];
+                }
+            }
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
